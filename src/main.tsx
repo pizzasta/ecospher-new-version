2419ceccd4aae88d234e4e17f804f3afe51b6979
pizzasta <jessbritt39@gmail.com>
2026-06-07 18:20:58 -0700

    Initialize main.tsx with React setup

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css'
+import './dead-zones.css'
+import './listening-rooms.css'
+import './anomalies.css'
+import './signal-alerts.css'
+import './settings.css'
+import './signal-inspirations.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
