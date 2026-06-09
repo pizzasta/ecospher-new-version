8b577631051c0babb09b88238dbb141a57d847ec
pizzasta <jessbritt39@gmail.com>
2026-06-08 22:50:09 -0700

    Import additional CSS files in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './design-tokens.css'
 import './styles.css'
+import './dead-zones.css'
+import './listening-rooms.css'
+import './anomalies.css'
+import './signal-alerts.css'
+import './settings.css'
+import './signal-inspirations.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
